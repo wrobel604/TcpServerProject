df902332c87c2f8626ca2d6a22ff8554941bea83
Michał Wróbel <wrobel604@hotmail.com>
2020-11-22 17:14:41 +0100

    Dodanie projektu biblioteki systemu logowania LosinSystem oraz projektu LoginSystemTest do testowanie biblioteki.

--- LoginSystemTest/Program.cs
+++ LoginSystemTest/Program.cs
@@ -8,7 +8,21 @@ namespace LoginSystemTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string host = "127.0.0.1";
+            if (args.Length > 0) { host = args[0]; }
+            int port = 5555;
+            if (args.Length > 1) { port = int.Parse(args[1]); }
+            try
+            {
+                TcpServer tcpServer = new TcpServer(host, port);
+                Console.WriteLine($"Uruchomiono serwer na adresie {host}:{port}");
+                tcpServer.OutputGenerateFunction = LoginSystem.LoginSystem.messageParser;
+                tcpServer.Listening();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
